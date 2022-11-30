ca8d7a068a1249e94b8bf1256b8f47808c1bf3f5
Jacob Martinson <jacobm3@gmail.com>
2022-11-29 22:28:49 -0600

    asdf

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -21,7 +21,7 @@ sudo apt-get install -y nmap bzip2 netcat net-tools git htop sysstat iotop vim-n
   ca-certificates curl gnupg lsb-release terraform vault zstd
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
