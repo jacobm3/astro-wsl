ebcfbbf6e08c5de79ff2d9a1333ad7ac174aedeb
Jacob Martinson <jacobm3@gmail.com>
2022-11-29 20:33:12 -0600

    asdf

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -10,27 +10,17 @@ echo '%sudo  ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 # Tell the package installer not to ask questions because this is a script
 export DEBIAN_FRONTEND=noninteractive
 
-# Add standard devops packages
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
 sudo apt-get update
-sudo apt-get install -y nmap bzip2 netcat net-tools git htop sysstat iotop vim-nox python3-pip jq lm-sensors terraform vault zstd
+sudo apt-get upgrade 
+sudo apt-get install -y nmap bzip2 netcat net-tools git htop sysstat iotop vim-nox python3-pip jq lm-sensors
 
 
-# Remove Ubuntu docker pkgs, Install Docker CE
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# https://docs.docker.com/engine/install/ubuntu/
+#
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 sudo usermod -G docker -a $USER
